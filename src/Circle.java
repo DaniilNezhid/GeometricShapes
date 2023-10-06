0eae6224cb518cbda04da27e7e585c6f95b49a83
danilnezid <danyhdmi@gmail.com>
2023-10-07 01:25:43 +0300

    FourCommit

--- src/Circle.java
+++ src/Circle.java
@@ -6,7 +6,6 @@ public class Circle implements Shape{
 
     @Override
     public double area() {
-        // нужно правильно называть поля что бы было понятно
         double area = Math.pow(radius,2);
         return (Math.PI*area);
     }
